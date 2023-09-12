60b6b82fc213e7d7639cb2298103fb9d86aad1ff
KalpaDS <desilvakalpa5@gmail.com>
2023-09-12 12:56:18 +0530

    #CSS and Cam setup

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import './App.css';
 import React, {Suspense} from "react";
 import {Canvas} from 'react-three-fiber';
 import {Car} from './Car';
-import {OrbitControls, PerspectiveCamera, CubeCamera} from '@react-three/drei';
+import {OrbitControls, PerspectiveCamera, CubeCamera, Environment} from '@react-three/drei';
 
 function App() {
     return (
         <>
             <Canvas>
                 <Suspense fallback={null}>
-
+                    <CubeCamera resolution={1024} frames={Infinity}>
+                        {(texture) => (
+                            <>
+                                <Environment map={texture}/>
+                                <Car receiveShadow
+                                     castShadow
+                                     envMapIntensity={20}
+                                     position={[0, 0.85, 0]}/>
+                            </>
+                        )}
+                    </CubeCamera>
                 </Suspense>
             </Canvas>
         </>
